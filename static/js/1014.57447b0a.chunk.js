c2907645565c92381bf5c27fc4681eb0d7274abb
haidoan9205 <haidoan9205@gmail.com>
2023-10-15 21:49:53 +0700

    Updates

---
+++ static/js/1014.57447b0a.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunk_coreui_coreui_free_react_admin_template=self.webpackChunk_coreui_coreui_free_react_admin_template||[]).push([[1014],{66265:function(e,s,r){r.r(s);r(72791);var c=r(92707),i=r(80184);s.default=()=>(0,i.jsxs)("div",{children:[(0,i.jsx)(c.S8,{}),(0,i.jsxs)("div",{className:"wrapper d-flex flex-column min-vh-100 bg-light",children:[(0,i.jsx)(c.tf,{}),(0,i.jsx)("div",{className:"body flex-grow-1 px-3",children:(0,i.jsx)(c.Lk,{})}),(0,i.jsx)(c.qh,{})]})]})}}]);
+//# sourceMappingURL=1014.57447b0a.chunk.js.map
